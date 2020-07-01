069c26a6a0fe6190af698d2a8eacb1b34bd270b0
Shintaro Sawada <szb03072@edu.osakafu-u.ac.jp>
2020-07-01 09:29:22 +0900

    fixed RobertaEmbedding. (considering contextualized embedding)

--- swan_sim/emb_model.py
+++ swan_sim/emb_model.py
@@ -8,7 +8,10 @@ from transformers import RobertaModel, RobertaTokenizer
 class RobertaEmbedding():
     def __init__(self):
         self.tok = RobertaTokenizer.from_pretrained("roberta-base")
+        self.special_tokens = ["[Mstart]", "[Mend]"]
+        self.tok.add_tokens(self.special_tokens)
         self.model = RobertaModel.from_pretrained("roberta-base")
+        self.model.resize_token_embeddings(len(self.tok))
         self.MAX_SEQ_LEN = 256
 
 
@@ -21,19 +24,37 @@ class RobertaEmbedding():
                                     max_length=self.MAX_SEQ_LEN,
                                     pad_to_max_length=self.MAX_SEQ_LEN) for t in speech_tokens]
 
-        return torch.tensor(speech_ids)
+        return speech_ids
+
+
+    def get_special_tokens_index_list(self, speech_ids):
+        Mstart_list = []
+        Mend_list = []
+        for index, sentence_id in enumerate(encode):
+            if sentence_id == self.special_tokens[0]:
+                Mstart_list.append(index)
+            elif sentence_id == self.special_tokens[1]:
+                Mend_list.append(index)
+
+        assert len(Mstart_list) == len(Mend_list), "must be same len."
+
+        return Mstart_list, Mend_list
 
 
     def roberta_embedding(self, speech):
         # get embeddings
-        input_roberta = self.roberta_encode(speech)
+        speech_ids = self.roberta_encode(speech)
+        input_roberta = torch.tensor(input_roberta)
+
+        Msart_list, Mend_list = self.get_special_tokens_index_list(speech_ids)
 
         with torch.no_grad():
             roberta_out = self.model(input_roberta)
-        embed_speech = roberta_out[1]
-        embed_speech_np = embed_speech.numpy()
+        embed_speech, _ = roberta_out
 
-        return embed_speech_np[0]
+        embed_adu = torch.stack([embed_speech[0][Mend_list[i]] - embed_speech[0][Msart_list[i]] for i in range(len(s1_list))])
+
+        return embed_adu.numpy()
 
 
     def __call__(self, speech):
